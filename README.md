fd0ce29ffae9ca4267f736d16bc59b86b2b5aa9e
Anthony Vumbaca <gitvumbaca@gmail.com>
2016-11-28 11:32:02 -0500

    finished Knights_travails

---
+++ README.md
@@ -0,0 +1,16 @@
+# Projects: Basic Data Structures and Algorithms
+
+From The Odin Project: http://www.theodinproject.com/courses/ruby-programming/lessons/data-structures-and-algorithms
+
+##Project 1: Searching Binary Trees
+
+Build a method (#build_tree) which takes an array of data and turns it into a binary tree full of Node objects appropriately placed. Then build the "crawler" function that will locate data inside of it. These would be the methods for #breadth_first_search, #depth_first_search and a recursive depth_first_search (#dfs_rec).
+
+-------
+
+##Project 2: Knight's Travails
+
+Build a method (#knight_moves) that shows the simplest possible way to get from one square on a 8x8 chess board to another square by outputting all squares the knight will stop on along the way. To do this, I had to create a tree data structure to hold all the possible knight positions. Once the target position was encountered, a breadth-first-search was called on the data tree to find the target and trace back to the starting position.
+*\* For me the trickiest part was figuring out that the data tree could not be binary (each node would have a bunch of child nodes, not just a left or right child node). At that point having to work with an array of child nodes (@children) was the challenge.*
+
+
--- knights_travails.rb
+++ knights_travails.rb
@@ -1,19 +1,3 @@
-# build tree using the starting square as the root node
-  # make method #find_moves that builds the moves, validates each move with #valid?, 
-  # all valid moves become child nodes to the parent node
-  #   Put child nodes into @children array
-
-  #   return the target square path if it finds a match
-  #   otherwise put all the valid moves into a Q and run #find_moves on .each of them.
-  #   need to put any node that has been run by #find_moves into a visited array so we dont check them again.
-
-# build a tree of moves
-# take the root position and build only the valid child positions
-#  only save those positions as square instances (maybe in an array together in @children)
-# check to see if any have the value of the target position (this could stop the tree building)
-
-
-
 class Square
   attr_accessor :value, :parent_node, :children
 
@@ -36,52 +20,42 @@ class KnightMoves
     @root = Square.new(root)
     @tree << @root
     @visited = [root]
-
-    build_tree(@root) # needed to put this method call after @visited so @visited gets initialized first.
+    build_tree([@root]) # needed to put this method call after @visited so @visited gets initialized first.
+                        # needed to keep this argument in array form in order to use .each in the #build_moves method.
   end
 
-  def build_tree(square)
-    puts square.inspect
-    
-    puts build_moves(square).inspect
-    puts @tree.inspect
-    # arr.each { |num| insert_node(@root, num) }
-    # puts @tree.inspect
+  def build_tree(square_arr)
+    build_moves(square_arr)
   end
 
-  def build_moves(square)
-    child_moves = []
-    x = square.value[0]
-    y = square.value[1]
-    child_moves.push([x + 2, y + 1], [x + 1, y + 2], [x -1, y + 2], [x - 2, y + 1], 
-                      [x - 2, y - 1], [x - 1, y -2], [x + 1, y - 2], [x + 2, y - 1])
-    
-    # takes array of valid child moves and puts each of them into @visited
-    valid_move?(child_moves).each { |move| 
-      @visited << move
-      child = Square.new(move, square)
-      square.children << child
+  def build_moves(square_arr)
+    square_arr.each { |square| 
+      child_moves = []
+      x = square.value[0]
+      y = square.value[1]
+      child_moves.push([x + 2, y + 1], [x + 1, y + 2], [x -1, y + 2], [x - 2, y + 1], 
+                        [x - 2, y - 1], [x - 1, y -2], [x + 1, y - 2], [x + 2, y - 1])
+      # each square value goes into @visited so we dont re-visit them again
+      @visited << square.value
+      valid_move?(child_moves).each { |move| 
+        # each valid child move goes into moves array to see if the target is matched later 
+        moves = [] 
+        moves << move
+        child = Square.new(move, square)
+        square.children << child
+        moves.include?(@target) ? return : build_moves(square.children)
+      }
     }
-     puts "this is @visited: #{@visited.inspect}"
-     puts "this is @children: #{square.inspect}"
   end
 
-  def valid_move?(arr)
+  def valid_move?(child_moves)
     valid_moves = []
-    arr.each { |arr| 
-      if arr[0].between?(1, 8) && arr[1].between?(1, 8)
+    child_moves.each { |arr| 
+      if arr[0].between?(1, 8) && arr[1].between?(1, 8) && !@visited.include?(arr)
         valid_moves << arr
       end
     }
     return valid_moves
-  end
-
-  def insert_node(node, num)
-    if num < node.value
-      node.child_left.nil? ? node.child_left = Node.new(num, node) : insert_node(node.child_left, num)
-    elsif num > node.value
-      node.child_right.nil? ? node.child_right = Node.new(num, node) : insert_node(node.child_right, num)
-    end
   end
 
   def breadth_first_search(value)
@@ -89,57 +63,33 @@ class KnightMoves
     until q.empty?
       node = q.shift
       if node.value == value
-        return "#{node} is equal to the value of #{value}."
+        return find_path(node)
       else
-        q << node.child_left unless node.child_left.nil?
-        q << node.child_right unless node.child_right.nil?
+        node.children.each { |child| 
+          q << child
+        }
       end
     end
-    nil
+  end
+
+  def find_path(node)
+    path = []
+    path_node = node
+    until path_node.parent_node.nil?
+      path << path_node.value
+      path_node = path_node.parent_node
+    end
+    path = (path << @root.value).reverse
+    puts "You made it in #{path.length} moves!!"
+    puts "Your path is:"
+    path.each { |move| puts move.inspect }
   end
 
 end
 
 def knight_moves(root, target)
   tree = KnightMoves.new(root, target)
-  # puts tree.inspect
+  tree.breadth_first_search(target)
 end
 
-
-puts knight_moves([1,1], [2,4])
-
-
-
-#arr = [5, 2, 4, 7, 8, 23]
-# bst = BinarySearchTree.new(arr)
-# puts bst.breadth_first_search(2)
-# puts bst.depth_first_search(23)
-# puts bst.dfs_rec(7)
-
-# def depth_first_search(value)
-#     stack = [@root]
-#     visited = []
-#     until stack.empty?
-#       node = stack[-1]
-#       if node.value == value
-#         return "#{node} is equal to the value of #{value}."
-#       else
-#         if node.child_left.nil? || visited.include?(node.child_left)
-#           visited << stack.pop
-#           stack << node.child_right unless node.child_right.nil?
-#         else
-#           stack << node.child_left
-#         end
-#       end
-#     end
-#     nil
-#   end
-
-#   def dfs_rec(value, node = @root)
-#     return "#{node} is equal to the value of #{value}." if node.value == value
-#     left = dfs_rec(value, node.child_left) unless node.child_left.nil?
-#     return left if left  # returns left if left is true
-#     right = dfs_rec(value, node.child_right) unless node.child_right.nil?
-#     return right if right  # returns right if right is true
-#     nil
-#   end
+knight_moves([1,1], [4,7])
